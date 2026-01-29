f15abe85a226803c1ac359b476d7ac914a969bc6
vygrigaravicius <v.grigaravicius@inbox.lt>
2026-01-29 15:47:35 +0000

    Event Routing impl. & checking username changes

--- src/Blocks.ECQ/Events/EventRouter.cs
+++ src/Blocks.ECQ/Events/EventRouter.cs
@@ -1,11 +1,23 @@
-﻿namespace Blocks.ECQ.Events
+﻿using System.Collections.Concurrent;
+
+namespace Blocks.ECQ.Events
 {
+    // TODO: Add Logging. 
     public sealed class EventRouter
     {
-        public async Task Route(IEventEnvelope @event, 
+        private static readonly ConcurrentDictionary<Type, 
+            IEventPipeline> _cache = [];
+
+        public async Task Route(IEventEnvelope @event,
             CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            IEventPipeline pipeline = _cache.GetOrAdd(@event.GetType(), 
+                Resolve(@event.GetType()));
+
+            await pipeline.Handle(@event, cancellationToken);
         }
+
+        private IEventPipeline Resolve(Type typeOfEvent)
+            => throw new NotImplementedException();
     }
 }
